4806789c6a1f8baf75b9ea981a49ef4888f18771
Selvaganesh velayutham <152356364+selvaganesh19@users.noreply.github.com>
2025-09-04 12:00:41 +0530

    Time: 9 ms (37.89%), Space: 13.3 MB (92.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,15 +5,4 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        p = 0
-        if len(nums) == 1:
-            return nums
-        for i in range(0, len(nums)):
-            if nums[i] != 0:
-                nums[p] = nums[i]
-                p += 1
-            
-        for k in range(p, len(nums)):
-            nums[k] = 0
-            
-        return nums
+        return nums.sort(key = lambda nums: nums ==0)
