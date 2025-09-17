ceb28d97d137ba1c9f33e0382c79e357389072f3
Selvaganesh velayutham <152356364+selvaganesh19@users.noreply.github.com>
2025-09-17 13:26:11 +0530

    Time: 1 ms (56.36%), Space: 12.5 MB (10.53%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def clearDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        res = ""
+
+        for i in s:
+            if i.isdigit():
+                res = res[:-1]
+            else:
+                res+=i
+        
+        return res
