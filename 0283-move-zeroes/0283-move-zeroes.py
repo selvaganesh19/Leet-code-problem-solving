68ce2d92c339934b0d962e1df7c974d87352e3eb
Selvaganesh velayutham <152356364+selvaganesh19@users.noreply.github.com>
2026-08-03 13:44:35 +0530

    Time: 8 ms (40.6%), Space: 13.6 MB (49.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,5 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        l=0
         
-        return nums.sort(key = lambda nums: nums ==0)
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[r],nums[l] = nums[l],nums[r]
+                l+=1
+                
