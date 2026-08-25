a6acaeeacd6863f552320f265e6dcdd326481bf4
Selvaganesh velayutham <152356364+selvaganesh19@users.noreply.github.com>
2026-08-25 09:49:20 +0530

    Time: 112 ms (21.77%), Space: 31 MB (70.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,14 +1,44 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        import heapq
-        count = 0
-        arr = []
-        for num in nums:
-            if count==k:
-                heapq.heappush(arr,num)
-                temp = heapq.heappop(arr)
+        target = len(nums) - k
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            pivot = nums[random.randint(left, right)]
+
+            # 3 sections:
+            # < pivot | == pivot | > pivot
+
+            low = left
+            mid = left
+            high = right
+
+            while mid <= high:
+
+                if nums[mid] < pivot:
+                    nums[low], nums[mid] = nums[mid], nums[low]
+                    low += 1
+                    mid += 1
+
+                elif nums[mid] > pivot:
+                    nums[mid], nums[high] = nums[high], nums[mid]
+                    high -= 1
+
+                else:
+                    mid += 1
+
+            # [left ... low-1]    < pivot
+            # [low  ... high]     == pivot
+            # [high+1 ... right]  > pivot
+
+            if target < low:
+                right = low - 1
+
+            elif target > high:
+                left = high + 1
+
             else:
-                heapq.heappush(arr,num)
-                count+=1
-        return arr[0]
-        
+                return nums[target]
